d470d36ab518424e22f8e3458d3f9b89402180a2
lrmello <leonardo.reis28051995@gmail.com>
2023-02-23 08:08:36 -0300

    Inclusão dos arquivos feitos recentemente

---
+++ app.py
@@ -0,0 +1,10 @@
+nome = "Leonardo" 
+idade = 21 
+altura = 1.81
+is_male = True
+gosto_de= "Beber"
+
+print(f"Meu nome é {nome}.")
+print(f"Eu gosto de {gosto_de}")
+print(f"Eu tenho {idade} anos")
+print(f"{nome} gosta de {gosto_de} e tem {idade} anos")
---
+++ app2.py
@@ -0,0 +1,41 @@
+import math
+
+num1 = 5
+num2 = 5.8
+
+print(type(num1))
+print(type(num2))
+
+a = float(num1)
+##print(a)
+##print(type(a))
+
+b = int(num2)
+##print(b)
+##print(type(b))
+
+##a = int("5")
+##b = "3.8"
+
+##print(type(a))
+##print(type(b))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
+print(num1 // num2)
+
+print(3 + 5 * 28 + 3)
+print((3 + 5) * (28 + 3))
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,5))
+print(min(6,10))
+print(round(8.4))
+print(math.floor(8.3820))
+print(math.ceil(8.0091))
+print(math.sqrt(81))
